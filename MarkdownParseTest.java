44504af575065f4d31ccc4cd12e0ad4962d69a78
fjiang316 <fejiang@ucsd.edu>
2022-04-22 10:56:34 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,8 +1,23 @@
 import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.nio.file.Path;
+import java.io.IOError;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+
 import org.junit.*;
 public class MarkdownParseTest {
     @Test
     public void addition(){
         assertEquals(2, 1+1);
+    }
+
+    @Test
+    public void testLinks() throws IOException {
+        Path fileName = Path.of("test-file.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> result = MarkdownParse.getLinks(content);
+        assertEquals(List.of("https://something.com", "some-thing.html"), result);
     }
 }
